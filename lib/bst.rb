7a7931e537b69bcf5d33129cdb42299f71217ad6
Tony Hoyin Liu <tonyhoyinliu@gmail.com>
2016-07-31 15:26:33 -0700

    progress

--- lib/bst.rb
+++ lib/bst.rb
@@ -11,19 +11,24 @@ end
 
 class BinarySearchTree
   def initialize
-
+    @root_node = nil
   end
 
   def insert(value)
+    unless @root_node
+      @root_node = BSTNode.new(value)
+      return
+    end
 
+    BinarySearchTree.insert!(@root_node, value)
   end
 
   def find(value)
-
+    BinarySearchTree.find!(@root_node, value)
   end
 
   def inorder
-
+    BinarySearchTree.inorder!(@root)
   end
 
   def postorder
@@ -51,11 +56,27 @@ class BinarySearchTree
   end
 
   def self.insert!(node, value)
+    return BSTNode.new(value) unless node
 
+    if value <= node.value
+      node.left = BinarySearchTree.insert!(node.left, value)
+    else
+      node.right = BinarySearchTree.insert!(node.right, value)
+    end
+
+    node
   end
 
   def self.find!(node, value)
+    # base cases
+    return nil unless node
+    return node if node.value == value
 
+    if value < node.value
+      return BinarySearchTree.find!(node.left, value)
+    end
+    
+    BinarySearchTree.find!(node.right, value)
   end
 
   def self.preorder!(node)
